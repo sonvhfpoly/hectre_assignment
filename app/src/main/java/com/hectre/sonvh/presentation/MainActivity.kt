ca62184cb25e12edce66d37d11c5ad91f85bf442
sonvh <sonvh@icheck.vn>
2022-06-12 21:15:02 +0700

    init

---
+++ app/src/main/java/com/hectre/sonvh/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hectre.sonvh.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hectre.sonvh.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
